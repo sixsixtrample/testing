93fbd43014a1a1b379e514257f163aa065dafa9f
andy <apoquette@gmail.com>
2014-02-13 20:30:54 -0600

    this is a test

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Created by andy on 2/13/14.
+ */
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200,{"Content-Type": "text/plain"});
+    res.end("Hello world\n");
+});
+
+
+server.listen(8000);
+
